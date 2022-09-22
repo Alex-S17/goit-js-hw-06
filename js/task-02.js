5682edb99a448f6c6f0cad9a6a45a1b07763c3e6
Sergey J <aleks.s.educ@gmail.com>
2022-09-22 15:47:31 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const list = document.querySelector('#ingredients');
+const arrayOfItems = [];
+
+for (const ingredient of ingredients) {
+  const currentItem = document.createElement('li');
+  currentItem.textContent = ingredient;
+  currentItem.classList.add('item');
+  arrayOfItems.push(currentItem);
+}
+
+list.append(...arrayOfItems);
